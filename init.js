fc04020bcd76fa1c1f35ad4f3059b904425bea12
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-10-14 06:41:36 -0300

    update

--- init.js
+++ init.js
@@ -41,53 +41,57 @@ async function createApp(defaultChange, API_PORT, FRONTEND_PORT) {
         }
     }
     
-      async function recargarNginx() {
-        const comando = "sudo systemctl reload nginx";
+    async function recargarNginx() {
+    const comando = "sudo systemctl reload nginx";
+
+    const { stdout, stderr } = await exec(comando);
+    console.log(`Resultado: ${stdout}`);
+    console.error(`Errores: ${stderr}`);
+    }
+
+    async function crearSubdominioCloudFlare(subdomain) {
+    const zoneId = "22ba6192a10c766dd77527c7a101ad35";
+    const apiKey = "77543657f985f75834e7951b022638892bddc";
+    const authEmail = "carlo.gammarota@gmail.com";
+    const dnsRecordData = {
+        type: "A",
+        name: subdomain,
+        content: "64.227.76.217",
+        ttl: 1,
+        proxied: true,
+    };
+
+    const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
+
+    const config = {
+        method: "post",
+        url: apiUrl,
+        headers: {
+        "X-Auth-Key": apiKey,
+        "X-Auth-Email": authEmail,
+        "Content-Type": "application/json",
+        },
+        data: dnsRecordData,
+    };
+
+    try {
+        const response = await axios(config);
+        console.log("Registro DNS agregado con éxito:", response.data);
+        
+        return response.data;
+    } catch (error) {
+        res.send("Error al agregar el registro DNS (CloudFlare)");
+        // console.error("Error al agregar el registro DNS:", error.errors);
+        // return "Error al agregar el registro DNS (CloudFlare)"
+        console.log("Error al agregar el registro DNS (CloudFlare)");
+        // throw new Error("Error al agregar el registro DNS (CloudFlare)");
+    }
+    }
     
-        const { stdout, stderr } = await exec(comando);
-        console.log(`Resultado: ${stdout}`);
-        console.error(`Errores: ${stderr}`);
-      }
-    
-      async function crearSubdominioCloudFlare(subdomain) {
-        const zoneId = "22ba6192a10c766dd77527c7a101ad35";
-        const apiKey = "77543657f985f75834e7951b022638892bddc";
-        const authEmail = "carlo.gammarota@gmail.com";
-        const dnsRecordData = {
-          type: "A",
-          name: subdomain,
-          content: "64.227.76.217",
-          ttl: 1,
-          proxied: true,
-        };
-    
-        const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
-    
-        const config = {
-          method: "post",
-          url: apiUrl,
-          headers: {
-            "X-Auth-Key": apiKey,
-            "X-Auth-Email": authEmail,
-            "Content-Type": "application/json",
-          },
-          data: dnsRecordData,
-        };
-    
-        try {
-          const response = await axios(config);
-          console.log("Registro DNS agregado con éxito:", response.data);
-          
-          return response.data;
-        } catch (error) {
-            res.send("Error al agregar el registro DNS (CloudFlare)");
-          // console.error("Error al agregar el registro DNS:", error.errors);
-          // return "Error al agregar el registro DNS (CloudFlare)"
-          console.log("Error al agregar el registro DNS (CloudFlare)");
-          // throw new Error("Error al agregar el registro DNS (CloudFlare)");
-        }
-      }
-    
+    await crearSubdominioCloudFlare(defaultChange);
+    console.log(defaultChange, API_PORT);
+    await clonarArchivoDominioDefault(defaultChange, API_PORT);
+    await recargarNginx();
 
   // Variable para almacenar el valor de defaultChange
   const terceraVariable = defaultChange;
@@ -217,14 +221,11 @@ async function createApp(defaultChange, API_PORT, FRONTEND_PORT) {
 
       console.log("Base de datos clonada exitosamente.");
       
-      try {
-        await crearSubdominioCloudFlare(defaultChange);
-        console.log(defaultChange, API_PORT);
-        await clonarArchivoDominioDefault(defaultChange, API_PORT);
-        await recargarNginx();
-    } catch (error) {
-        console.error("Error:", error.message);
-    }
+    //   try {
+      
+    // } catch (error) {
+        // console.error("Error:", error.message);
+    // }
       init();
     } catch (error) {
       console.error("Error al clonar la base de datos:", error);
