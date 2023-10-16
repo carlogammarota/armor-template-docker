9c000a6c3c01bec5776c1563321019534599ed2a
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-10-16 20:17:20 -0300

    update

--- init.js
+++ init.js
@@ -21,6 +21,38 @@ app.use(bodyParser.json());
 async function createApp(defaultChange, API_PORT, FRONTEND_PORT) {
 
 
+
+
+  function editarYGuardar(serverModeloPath, dominioNuevo, subdominioNuevo) {
+    fs.readFile(serverModeloPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo:', err);
+            return;
+        }
+
+        // Realizar las sustituciones globales
+        let result = data.replace(/subdominioEdit/g, subdominioNuevo);
+        // let result = data.replace(/dominioEdit/g, dominioNuevo).replace(/subdominioEdit/g, "9999");
+
+        // Guardar el archivo editado
+        fs.writeFile('./frontend/server.js', result, 'utf8', (err) => {
+            if (err) {
+                console.error('Error al escribir el archivo:', err);
+                return;
+            }
+            console.log('Archivo guardado exitosamente.');
+        });
+    });
+}
+
+// Uso de la función
+const serverModeloPath = './serverModelo.js';
+const dominioNuevo = 'nuevoDominio';
+const subdominioNuevo = 'tesla';
+
+editarYGuardar(serverModeloPath, dominioNuevo, defaultChange);
+
+
     function clonarArchivoDominioDefault(subdomain, port) {
         const archivoDefault = "domain-default.conf";
         const nuevoNombre = `${subdomain}.armortemplate.site`;
@@ -150,35 +182,6 @@ async function createApp(defaultChange, API_PORT, FRONTEND_PORT) {
 
 
 
-
-  function editarYGuardar(serverModeloPath, dominioNuevo, subdominioNuevo) {
-    fs.readFile(serverModeloPath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo:', err);
-            return;
-        }
-
-        // Realizar las sustituciones globales
-        let result = data.replace(/subdominioEdit/g, subdominioNuevo);
-        // let result = data.replace(/dominioEdit/g, dominioNuevo).replace(/subdominioEdit/g, "9999");
-
-        // Guardar el archivo editado
-        fs.writeFile('./frontend/server.js', result, 'utf8', (err) => {
-            if (err) {
-                console.error('Error al escribir el archivo:', err);
-                return;
-            }
-            console.log('Archivo guardado exitosamente.');
-        });
-    });
-}
-
-// Uso de la función
-const serverModeloPath = './serverModelo.js';
-const dominioNuevo = 'nuevoDominio';
-const subdominioNuevo = 'tesla';
-
-editarYGuardar(serverModeloPath, dominioNuevo, defaultChange);
 
 
 
